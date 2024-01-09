750d7d62213b7b65d6c851547355ad0cb83c372b
prajapatisweta <s.p.2410.s.p@gmail.com>
2024-01-09 22:11:58 +0530

    leaflet marker

--- script.js
+++ script.js
@@ -30,15 +30,32 @@ if (navigator.geolocation)
             const { longitude } = position.coords;
             console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
-            const map = L.map('map').setView([51.505, -0.09], 13);
+            const coords = [latitude, longitude];
 
-            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            const map = L.map('map').setView(coords, 16);
+
+            // console.log(map);
+
+            L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             }).addTo(map);
+            
+            map.on('click', function(mapEvent) {
+                console.log(mapEvent);
+                const {lat, lng} = mapEvent.latlng;
 
-            L.marker([51.5, -0.09]).addTo(map)
-                .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+                L.marker([lat, lng])
+                .addTo(map)
+                .bindPopup(L.popup({
+                    maxWidth: 250,
+                    minWidth: 100,
+                    autoClose: false,
+                    closeOnClick: false,
+                    className: 'running-popup',
+                }))
+                .setPopupContent('Workout')
                 .openPopup();
+            });
 
         },
         function () {
